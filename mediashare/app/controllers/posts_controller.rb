1dea4862038123a7d33550e09dbdf3f4f4648f92
Squarix <vlas2305@gmail.com>
2019-01-20 00:41:33 +0300

    Completed posts

--- mediashare/app/controllers/posts_controller.rb
+++ mediashare/app/controllers/posts_controller.rb
@@ -1,10 +1,22 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
-  before_action :authenticate_user!, except: %i[index show]
+  before_action :authenticate_user!, except: %i[index show autocomplete]
   before_action :get_post, only: %i[show edit update destroy]
+  before_action :force_json, only: :autocomplete
+  
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 10)
+    @posts = Post.do_search(params)
+  end
+
+  def autocomplete
+    @posts = Post.search('title: ' + params[:query], :per_page => 5, boost: 1).results().map do |r|
+      r = { :name => r.title, :url => '/posts/' + r.id }
+    end
+    @users = User.search('login: ' + params[:query], :per_page => 5, :boost => 1).results().map do |u|
+      u = { :name => u.login, :url => '/users/' + u.id }
+    end
+    render json: {posts: @posts, users: @users}  
   end
 
   def new
@@ -13,31 +25,52 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    redirect_to index_home_path, success: 'Пост успешно удален'
+    redirect_to :root, success: 'Post has been removed'
   end
 
-  def edit; end
-
-  def update
-    if @post.update_attributes(post_params)
-      redirect_to @post, success: 'Пост успешно обновлен'
-    else
-      render :edit, danger: 'Пост не был обновлен'
+  def edit
+    unless current_user.id == @post.user.id 
+      unless current_user.admin
+        redirect_to :root, danger: 'Access denied'
+      end
     end
   end
 
-  def show; end
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to @post, success: 'Post has been updated'
+    else
+      render :edit, danger: 'Post was not updated'
+    end
+  end
+
+  def show
+    @comments = @post.comments
+  end
 
   def create
     @post = Post.new(post_params)
     if @post.save
-      redirect_to @post, success: 'Пост успешно создан'
+      redirect_to @post, success: 'Success'
     else
-      render :new, danger: 'Пост не был создан'
+      render :new, danger: 'Post was not created'
     end
   end
 
+  def show_comments(comments)
+    unless comments.nil? 
+      comments.map do |c|
+        puts c.body.to_s
+      end
+    end
+    return 
+  end
+  helper_method :show_comments
   private
+
+  def force_json
+    request.format = :json
+  end
 
   def get_post
     @post = Post.find(params[:id])
--- mediashare/app/models/post.rb
+++ mediashare/app/models/post.rb
@@ -1,22 +1,67 @@
 # frozen_string_literal: true
+require 'elasticsearch/model'
 
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :taggings
+  has_many :comments, :as => :commentable
   belongs_to :user
   has_many :tags, through: :taggings
   belongs_to :category
 
   validates :title, :image, :user_id, presence: true
 
+  include Tire::Model::Search
+  include Tire::Model::Callbacks
+  include Elasticsearch::Model
+
+  mapping do
+    indexes :id, type: 'integer'
+    indexes :user_id, type: 'integer'
+    indexes :user_name
+    indexes :category_id, type: 'integer'
+    indexes :category_name
+    indexes :title, boost: 10
+    indexes :description
+    indexes :created_at, type: 'integer'
+  end
+
   def all_tags
-    tags.map(&:name).join(', ')
+    tags.map(&:name).join(' ')
   end
 
   def all_tags=(names)
-    self.tags = names.split(',').map do |name|
+    self.tags = names.split(%r{#* }).map do |name|
       Tag.where(name: name.strip).first_or_create!
     end
+  end
+
+  def self.do_search(params)
+    tire.search(load: true, page: params[:page], per_page: 4) do
+      query do
+        boolean do
+          must { string params[:query], default_operator: 'AND' } if params[:query].present?
+          must { range :created_at, lte: Time.zone.now }
+          must { term :category_id, params[:category_id] } if params[:category_id].present?
+        end
+      end
+      sort { by :created_at, 'desc' } if params[:query].blank?
+      facet 'categories' do
+        terms :category_id
+      end
+    end
+  end
+
+  def to_indexed_json
+    to_json(methods: [:category_name])
+  end
+
+  def category_name
+    category.name
+  end
+
+  def user_name
+    user.email
   end
 end
